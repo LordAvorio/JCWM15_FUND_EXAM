ff98eede1234855695fb3ca88b1abc5d3ab0a963
LordAvorio <gadingramjets@gmail.com>
2020-11-16 11:55:58 +0700

    gading : ujian fundamental JCWM modified

--- gading-jcwm-15/js/soal1.js
+++ gading-jcwm-15/js/soal1.js
@@ -3,6 +3,7 @@ function cariPattern(event){
 
     console.log("button diklik")
 
+    let fieldAngka = document.getElementById("inputAngka")
     let angka = parseInt(document.getElementById("inputAngka").value)
     let tipe = document.getElementById("inputTipe").value
 
@@ -10,28 +11,31 @@ function cariPattern(event){
     var temp = `<p>`
     let x = 1
 
-
-    if(tipe == "ganjil"){
-        for (let i = 0; i <angka; i++){
-            for (let j = 0; j <= i; j++){
-                temp += `${x} `
-                x += 2
+    if(fieldAngka == ""){
+        alert("Inputan tidak boleh kosong")
+    }else if(angka > 4 || angka <= 0){
+        alert("Inputan max 4 dan tidak boleh minus")
+    }else{
+        if(tipe == "ganjil"){
+            for (let i = 0; i <angka; i++){
+                for (let j = 0; j <= i; j++){
+                    temp += `${x} `
+                    x += 2
+                }
+                temp += `</br>`
             }
-            temp += `</br>`
-        }
-        temp += `</p>`
-        document.getElementById("hasilPattern").innerHTML = `${temp}`
-    }else if(tipe == "huruf"){
-        for (let i = 0; i <angka; i++){
-            for (let j = 0; j <= i; j++){
-                temp += `${abjad[x]} `
-                x += 2
+            temp += `</p>`
+            document.getElementById("hasilPattern").innerHTML = `${temp}`
+        }else if(tipe == "huruf"){
+            for (let i = 0; i <angka; i++){
+                for (let j = 0; j <= i; j++){
+                    temp += `${abjad[x]} `
+                    x += 2
+                }
+                temp += `</br>`
             }
-            temp += `</br>`
+            temp += `</p>`
+            document.getElementById("hasilPattern").innerHTML = `${temp}`
         }
-        temp += `</p>`
-        document.getElementById("hasilPattern").innerHTML = `${temp}`
     }
-
-
 }
